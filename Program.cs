1c14dea71c87d621d96985a62403524c9f330e01
Samuel Bacellar <samuelbacellar1@gmail.com>
2022-04-09 16:19:15 -0300

    Criando o código do projeto
    Programação em C# (orientada a objetos) da Área de um Retângulo

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaRetangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo retangulo = new Retangulo();
+            Console.WriteLine("Calcula a area de um retângulo");
+            Console.Write("Informe a altura do retângulo: ");
+            retangulo.Altura = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Informe a base/largura do retângulo: ");
+            retangulo.Base = Convert.ToDouble(Console.ReadLine());
+            retangulo.ExibeDados();
+
+            Console.ReadKey();
+        }
+    }
+}
